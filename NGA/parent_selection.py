2d951a04d0736fcda3e3484dda4834796f89deee
KQ1113 <78833626+KQ1113@users.noreply.github.com>
2022-02-17 20:31:09 -0500

    Update parent_selection.py

--- NGA/parent_selection.py
+++ NGA/parent_selection.py
@@ -1,25 +1,16 @@
-import random
+'''
+This function use tournament to select a parent
+'''
+def tournament(size):
+    # randomly find some individual to join the tournament
+    parent = []
+    Index = list(range(len(build.network)))
+    parent_list = random.sample(Index, size)
+    # use the best individual in the list as parent.
+    parent_fit = float('inf')
+    for i in parent_list:
+        if build.network[i].fitness < parent_fit:
+            parent_fit = build.network[i].fitness
+            parent = build.network[i]
 
-
-def tournament(population,fitness,size):
-    Index = list(range(len(population)))
-    parent1_list = random.sample(Index,size)
-    parent1_fit = float('inf')
-    for i in parent1_list:
-        if fitness[i] < parent1_fit:
-            parent1_fit = fitness[i]
-            parent1 = population[i]
-
-    same_parent = True
-    while same_parent:
-        parent2_list = random.sample(Index,size)
-        parent2_fit = float('inf')
-
-        for j in parent2_list:
-            if fitness[j] < parent2_fit:
-                parent2_fit = fitness[j]
-                parent2 = population[j]
-        if parent1 != parent2:
-            same_parent = False
-
-    return parent1,parent2
+    return parent
