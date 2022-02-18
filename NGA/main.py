4dc80907762213acabf302a9779e48a2f7b81d9f
KQ1113 <78833626+KQ1113@users.noreply.github.com>
2022-02-17 20:28:43 -0500

    Update main.py

--- NGA/main.py
+++ NGA/main.py
@@ -7,47 +7,41 @@ import offspring_selection
 import reproduce
 
 population_size = 100
-dimension = 10
+dimension = 30
 Range = [-100, 100]
 tournament_size = 4
-generation = 50
+generation = 1000
 m = 4
 m0 = 4
 
 
-def next_generation(population, fitness):
-    parent1, parent2 = parent_selection.tournament(population, fitness, tournament_size)
-    #print(parent1, parent2)
-    offspring1, offspring2 = reproduce.crossover(parent1, parent2)
-    #print(offspring1, offspring2)
+def next_generation(size):
+    # do the parent selection
+    parent = parent_selection.tournament(size)
+
+    offspring1, offspring2 = reproduce.crossover(parent)
+
+    # mutate two offsprings and add them into network
     offspring1 = reproduce.mutation(offspring1, Range[0], Range[1])
     offspring2 = reproduce.mutation(offspring2, Range[0], Range[1])
-    #print(offspring1, offspring2)
-    offspring = [offspring1, offspring2]
-    offspring_fitness = evaluation.evaluate(offspring)
-    population, fitness = offspring_selection.mu_plus_lambda(population, fitness, offspring, offspring_fitness)
-    return population, fitness
+    build.add(offspring1)
+    build.add(offspring2)
+
+    # do the offspring selection
+    offspring_selection.remove_worst()
 
 
 def main():
     population = initialization.initialization(dimension, Range[0], Range[1], population_size)
-    # Here test for initialization
-    print("Test initialization")
-    for n in build.network:
-        print("id:", n.id, ", neighbour:", n.neighbour, ", fitness:", n.fitness)
-    # Stop here
-    exit()
+    # let the population loop through generation
     for i in range(generation):
-        population, fitness = next_generation(population, fitness)
-    #print(population)
-    print(fitness)
-    best_vector = []
-    best_fitness = float('inf')
-    for k in range(len(fitness)):
-        if fitness[k] <= best_fitness:
-            best_vector = population[k]
-            best_fitness = fitness[k]
-    print(best_fitness)
+        next_generation(tournament_size)
+
+    # print the best individual
+    net = build.network.copy()
+    net.sort(key=lambda x: x.fitness, reverse=True)
+    print("The best individual is:", net[0].id, ", val:", net[0].val)
+    print("The result is:", 1 / net[0].fitness)
+
+
 main()
-
-
