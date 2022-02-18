984ae3da2c498002d4b0916c4b6dfd2505d8a0e9
KQ1113 <78833626+KQ1113@users.noreply.github.com>
2022-02-17 20:29:20 -0500

    Update reproduce.py

--- NGA/reproduce.py
+++ NGA/reproduce.py
@@ -1,18 +1,27 @@
 import random
+import build
 
 
-def crossover(parent1, parent2):
-    offspring1 = []
-    offspring2 = []
+def crossover(parent1):
+    # get the index of all the neighbour of parent1
+    parent2_list = parent1.neighbour
+    # select one neighbour as parent2
+    parent2_id = random.choice(parent2_list)
+    for i in build.network:
+        if i.id == parent2_id:
+            parent2 = i
+    # do the single point crossover
+    dimension = len(parent1.val)
+    crossover_point = random.randint(0, dimension)
+    offspring1_val = parent1.val[0:crossover_point] + parent2.val[crossover_point:dimension]
+    offspring2_val = parent2.val[0:crossover_point] + parent1.val[crossover_point:dimension]
 
-    crossover_point = random.randint(0, len(parent1))
-
-    offspring1 = parent1[0:crossover_point] + parent2[crossover_point:len(parent2)]
-    offspring2 = parent2[0:crossover_point] + parent1[crossover_point:len(parent2)]
-
-    return offspring1, offspring2
+    return offspring1_val, offspring2_val
 
 
+'''
+This function do the single point mutation to the offspring
+'''
 def mutation(offspring, upper_range, lower_range):
     index = random.randint(0, len(offspring) - 1)
     offspring[index] = random.uniform(upper_range, lower_range)
