f019fcb7e0cb1817e54beac20b896daa484ac370
KQ1113 <78833626+KQ1113@users.noreply.github.com>
2022-02-17 20:28:58 -0500

    Update offspring_selection.py

--- NGA/offspring_selection.py
+++ NGA/offspring_selection.py
@@ -1,22 +1,24 @@
-def get_second(fit_info):
-    return fit_info[1]
+import build
 
 
-def mu_plus_lambda(population, population_fitness, offspring, offspring_fitness):
-    population_offspring = population + offspring
-    population_offspring_fitness = population_fitness + offspring_fitness
-    PopWithFit = []
-    for i in range(len(population_offspring)):
-        PopWithFit.append([population_offspring[i], population_offspring_fitness[i]])
+def remove_worst():
+    # sort the net
+    net = build.network.copy()
+    net.sort(key=lambda x: x.fitness)
+    remove_number = 2
+    # remove the ones with the worst fitness
+    while remove_number != 0:
+        build.remove(net[0].id)
+        net.pop(0)
+        remove_number -= 1
 
-    PopWithFit.sort(key=get_second)
-    NewPopWithFit = PopWithFit[0:len(population)]
-
-    new_population = []
-    new_fitness = []
-
-    for individual in NewPopWithFit:
-        new_population.append(individual[0])
-        new_fitness.append(individual[1])
-
-    return new_population, new_fitness
+    # find all the vector have no neighbour
+    no_neighbour = []
+    for vector in build.network:
+        if len(vector.neighbour) == 0:
+            no_neighbour.append(vector)
+    # relink them into the net
+    while len(no_neighbour) != 0:
+        build.remove(no_neighbour[0].id)
+        build.add(no_neighbour[0].val)
+        no_neighbour.pop(0)
