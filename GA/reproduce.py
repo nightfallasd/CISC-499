87a72e037beaa33acc53232e53ea3e901b983f6e
KQ1113 <78833626+KQ1113@users.noreply.github.com>
2022-03-20 19:41:31 -0400

    Update reproduce.py

--- GA/reproduce.py
+++ GA/reproduce.py
@@ -1,18 +1,24 @@
 import random
 
+"""
+This function do the single point crossover
+"""
+
 
 def crossover(parent1, parent2):
     offspring1 = []
     offspring2 = []
-
+    #choose a point to cross
     crossover_point = random.randint(0, len(parent1))
-
+    #generate offspring
     offspring1 = parent1[0:crossover_point] + parent2[crossover_point:len(parent2)]
     offspring2 = parent2[0:crossover_point] + parent1[crossover_point:len(parent2)]
 
     return offspring1, offspring2
 
-
+"""
+This function would do single point mutation
+"""
 def mutation(offspring, upper_range, lower_range, mutation_rate):
     if random.random() <= mutation_rate:
         index = random.randint(0, len(offspring) - 1)
