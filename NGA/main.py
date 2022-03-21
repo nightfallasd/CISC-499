b8f884e713c8dd300cfee982a2126fc5cc0c27fc
KQ1113 <78833626+KQ1113@users.noreply.github.com>
2022-03-20 23:37:13 -0400

    Update main.py

--- NGA/main.py
+++ NGA/main.py
@@ -6,42 +6,52 @@ import build
 import offspring_selection
 import reproduce
 
-population_size = 100
+population_size = 125
 dimension = 30
-Range = [-100, 100]
-tournament_size = 4
-generation = 1000
+Range = [-600, 600]
+tournament_size = 5
+generation = 2000
+parent_number = 20
+mutation_rate = 0.9
 m = 4
 m0 = 4
 
 
 def next_generation(size):
     # do the parent selection
-    parent = parent_selection.tournament(size)
-
-    offspring1, offspring2 = reproduce.crossover(parent)
+    offspring_list = []
+    for i in range(parent_number):
+        #parent = parent_selection.tournament(size)
+        parent = parent_selection.roulette()
+        offspring1, offspring2 = reproduce.crossover(parent)
 
     # mutate two offsprings and add them into network
-    offspring1 = reproduce.mutation(offspring1, Range[0], Range[1])
-    offspring2 = reproduce.mutation(offspring2, Range[0], Range[1])
-    build.add(offspring1)
-    build.add(offspring2)
+        offspring1 = reproduce.mutation(offspring1, Range[0], Range[1], mutation_rate)
+        offspring2 = reproduce.mutation(offspring2, Range[0], Range[1], mutation_rate)
+        offspring_list.append(offspring1)
+        offspring_list.append(offspring2)
+    for offspring in offspring_list:
+        build.add(offspring)
 
     # do the offspring selection
-    offspring_selection.remove_worst()
+    offspring_selection.remove_worst(parent_number*2)
 
 
 def main():
     population = initialization.initialization(dimension, Range[0], Range[1], population_size)
     # let the population loop through generation
+    generation_number = 0
     for i in range(generation):
+        generation_number += 1
         next_generation(tournament_size)
 
-    # print the best individual
-    net = build.network.copy()
-    net.sort(key=lambda x: x.fitness, reverse=True)
-    print("The best individual is:", net[0].id, ", val:", net[0].val)
-    print("The result is:", 1 / net[0].fitness)
+        if generation_number % 400 == 0:
+            # print the best individual
+            net = build.network.copy()
+            net.sort(key=lambda x: x.fitness, reverse=True)
+            print("After", generation_number, "generation:")
+            print("The best individual is:", net[0].id, ", val:", net[0].val)
+            print("The result is:", 1 / net[0].fitness)
 
 
 main()
