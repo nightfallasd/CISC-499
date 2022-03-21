c0e464e50446d415f084c7981c9440095ec16d24
KQ1113 <78833626+KQ1113@users.noreply.github.com>
2022-03-21 17:33:46 -0400

    Update evaluation.py

--- NGA/evaluation.py
+++ NGA/evaluation.py
@@ -1,13 +1,15 @@
+import math
+import random
+
 """
 This file evaluates fitness according to some benchmark functions in GA
 Modified by Yizhou Xiao
 """
 
 
-import math
-import random
-
-
+"""
+#range is [-32. 32]
+"""
 def f1(vector):
     D = len(vector)
     result = 0
@@ -25,6 +27,9 @@ def f1(vector):
         return 1/ abs(result)
 
 
+"""
+#range is [-100. 100]
+"""
 def f2(vector):
     result = 0
     for value in vector:
@@ -35,6 +40,9 @@ def f2(vector):
         return 1 / abs(result)
 
 
+"""
+#range is [-10, 10]
+"""
 def f3(vector):
     num1 = 0
     num2 = 0
@@ -48,16 +56,22 @@ def f3(vector):
         return 1 / abs(result)
 
 
+"""
+#range is [-1.28. 1.28]
+"""
 def f4(vector):
     result = 0
     for i in range(len(vector)):
-        result += i * vector[i] ** 4 + random.random()
+        result += (i+1) * vector[i] ** 4 + random.random()
     if result == 0:
         return 0
     else:
         return 1 / abs(result)
 
 
+"""
+#range is [-5.12, 5.12]
+"""
 def f5(vector):
     result = 0
     for value in vector:
@@ -68,19 +82,25 @@ def f5(vector):
         return 1 / abs(result)
 
 
+"""
+#range is [-600. 600]
+"""
 def f6(vector):
     num1 = 0
     num2 = 1
     for i in range(len(vector)):
         num1 += vector[i] ** 2
-        num2 *= math.cos(vector[i] / (math.sqrt(i) + 1))
-    result = num1 / 4000 + num2 + 1
+        num2 *= math.cos(vector[i] / (math.sqrt(i+1)))+1
+    result = num1 / 4000 + num2
     if result == 0:
         return 0
     else:
         return 1 / abs(result)
 
 
+"""
+#range is [-32. 32]
+"""
 def f7(vector):
     result = 0
     for i in range(len(vector) - 1):
@@ -108,5 +128,5 @@ The following function evaluates the fitness for a list of vectors
 def evaluate_lst(population):
     fitness = []
     for vector in population:
-        fitness.append(f2(vector))
+        fitness.append(f6(vector))
     return fitness
