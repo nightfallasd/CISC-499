d39780ffe5ea6c02721829b67998d10a1b8e5ace
KQ1113 <78833626+KQ1113@users.noreply.github.com>
2022-03-29 00:20:59 -0400

    Update and rename GA/evaluation.py to GA/evaluahttps:/github.com/nightfallasd/CISC-499/tree/main/GAtion.py

--- GA/evaluahttps:/github.com/nightfallasd/CISC-499/tree/main/GAtion.py
+++ GA/evaluahttps:/github.com/nightfallasd/CISC-499/tree/main/GAtion.py
@@ -7,6 +7,9 @@ This file evaluates fitness according to some benchmark functions in GA
 """
 
 
+"""
+#range is [-32, 32]
+"""
 def f1(vector):
     D = len(vector)
     result = 0
@@ -17,13 +20,16 @@ def f1(vector):
         num2 += math.cos(2 * math.pi * value)
     minuend = -20 * math.exp(-0.2 * math.sqrt(num1 / D))
     subtrahend = math.exp(num2 / D)
-    result = minuend - subtrahend + 20 + math.e
+    result = minuend - subtrahend
     if result == 0:
         return 0
     else:
         return 1 / abs(result)
 
 
+"""
+#range is [-100, 100]
+"""
 def f2(vector):
     result = 0
     for value in vector:
@@ -34,12 +40,15 @@ def f2(vector):
         return 1 / abs(result)
 
 
+"""
+#range is [-10, 10]
+"""
 def f3(vector):
     num1 = 0
     num2 = 0
     for value in vector:
-        num1 = abs(num1 + value)
-        num2 = abs(num2 * value)
+        num1 += abs(value)
+        num2 *= abs(value)
     result = num1 + num2
     if result == 0:
         return 0
@@ -47,16 +56,22 @@ def f3(vector):
         return 1 / abs(result)
 
 
+"""
+#range is [-1.28, 1.28]
+"""
 def f4(vector):
     result = 0
     for i in range(len(vector)):
-        result += i * vector[i] ** 4 + random.random()
+        result += (i+1) * vector[i] ** 4 + random.random()
     if result == 0:
         return 0
     else:
         return 1 / abs(result)
 
 
+"""
+#range is [-5.12, 5.12]
+"""
 def f5(vector):
     result = 0
     for value in vector:
@@ -67,19 +82,25 @@ def f5(vector):
         return 1 / abs(result)
 
 
+"""
+#range is [-600, 600]
+"""
 def f6(vector):
     num1 = 0
     num2 = 1
     for i in range(len(vector)):
         num1 += vector[i] ** 2
-        num2 *= math.cos(vector[i] / (math.sqrt(i) + 1))
-    result = num1 / 4000 + num2 + 1
+        num2 *= math.cos(vector[i] / (math.sqrt(i+1)))+1
+    result = num1 / 4000 - num2
     if result == 0:
         return 0
     else:
         return 1 / abs(result)
 
 
+"""
+#range is [-32, 32]
+"""
 def f7(vector):
     result = 0
     for i in range(len(vector) - 1):
